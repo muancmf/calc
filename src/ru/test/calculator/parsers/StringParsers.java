44b8193549cdc4e0daff68bc6b159d1690d5dc2c
Boris Naumkin <b.naumkin@lipt-soft.ru>
2021-04-19 21:29:25 +0300

    parseStringToLexemes optimized for multi symbol operations

--- src/ru/test/calculator/parsers/StringParsers.java
+++ src/ru/test/calculator/parsers/StringParsers.java
@@ -5,15 +5,14 @@ import org.springframework.stereotype.Component;
 import ru.test.calculator.lexemes.Lexeme;
 import ru.test.calculator.lexemes.operand.Operand;
 import ru.test.calculator.lexemes.operand.OperandImpl;
-import ru.test.calculator.lexemes.operation.Operation;
-import ru.test.calculator.lexemes.prioritysymbol.PrioritySymbol;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Component
 public class StringParsers {
-    private static final List<String> DELIMETERS = Arrays.asList(",", ".");
-    private static final String SYS_DELIMETER = ".";
+    private static final List<String> DELIMITERS = Arrays.asList(",", ".");
+    private static final String SYS_DELIMITER = ".";
 
     private Collection<Lexeme> lexemeOptions;
 
@@ -23,33 +22,50 @@ public class StringParsers {
     }
 
     public Collection<Lexeme> parseStringToLexemes(String input) {
-        Collection<Lexeme> result = new ArrayList<>();
-        String multiSymbolLexemeBuffer = "";
+        List<Object> digitsAndLexemes = new ArrayList<>();
+        digitsAndLexemes.add(input);
 
-        for (String symbol : input.split("")) {
-            Optional<Lexeme> lexeme = this.lexemeOptions.stream().filter(l -> l.getPresentation().equals(symbol)).findFirst();
-            if (lexeme.isPresent()) {
-                if (multiSymbolLexemeBuffer.length() > 0) {
-                    result.add(operandWithValue(multiSymbolLexemeBuffer));
-                    multiSymbolLexemeBuffer = "";
-                }
-                result.add(lexeme.get());
-            } else {
-                if (DELIMETERS.contains(symbol)) {
-                    multiSymbolLexemeBuffer += SYS_DELIMETER;
+        for (Lexeme lexeme : this.lexemeOptions) {
+            List<Object> subResult = new ArrayList<>();
+            for (Object item : digitsAndLexemes) {
+                if (item.getClass() == String.class) {
+                    String tail = (String) item;
+                    while (tail.length() > 0) {
+                        int index = tail.indexOf(lexeme.getPresentation());
+                        if (index >= 0) {
+                            if (index > 0) {
+                                subResult.add(tail.substring(0, index));
+                            }
+                            tail = tail.substring(index + lexeme.getPresentation().length());
+                            subResult.add(lexeme);
+                        } else {
+                            subResult.add(tail);
+                            tail = "";
+                        }
+                    }
                 } else {
-                    multiSymbolLexemeBuffer += symbol;
+                    subResult.add(item);
                 }
             }
-        }
-        if (multiSymbolLexemeBuffer.length() > 0) {
-            result.add(operandWithValue(multiSymbolLexemeBuffer));
+            digitsAndLexemes.clear();
+            digitsAndLexemes.addAll(subResult);
         }
 
+        Collection<Lexeme> result = new ArrayList<>();
+        for (Object lexeme : digitsAndLexemes) {
+            if (lexeme.getClass() == String.class) {
+                result.add(operandWithValue((String) lexeme));
+            } else {
+                result.add((Lexeme) lexeme);
+            }
+        }
         return result;
     }
 
     private Operand operandWithValue(String value) {
+        value = Arrays.stream(value.split(""))
+                .map(c -> DELIMITERS.contains(c) ? SYS_DELIMITER : c)
+                .collect(Collectors.joining());
         return new OperandImpl(Double.parseDouble(value));
     }
 }
